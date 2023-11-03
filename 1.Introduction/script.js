b0fb4a9b3023edd4e89cb8c58c73d55dff6321cc
MohammedAB <albay.m@hotmail.com>
2023-11-03 03:35:36 +0100

    Add solution for changing backgroundColor of body to hot pink

--- 1.Introduction/script.js
+++ 1.Introduction/script.js
@@ -1,6 +1,9 @@
 // TODO: Display the document title in the console
 const Title = document.getElementsByTagName("title")[0];
-console.log(Title);
+console.log(Title); // Or just: console.log(document.title); ??
 
 // TODO: Change the document title to "Modifying the DOM"
-Title.innerText = "Modifying the DOM";
+Title.innerText = "Modifying the DOM"; // Or just: document.title = "Modifying the DOM"; ??
+
+// TODO: Change the background color of the body to hot pink (#FF69B4)
+document.body.style.backgroundColor = "#FF69B4";
