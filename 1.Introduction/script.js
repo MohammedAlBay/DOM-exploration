d88f3a3acb3a5acf1047169b7820758d1285a856
MohammedAB <albay.m@hotmail.com>
2023-11-03 03:20:43 +0100

    Add solution to change document title

--- 1.Introduction/script.js
+++ 1.Introduction/script.js
@@ -1,3 +1,6 @@
 // TODO: Display the document title in the console
 const Title = document.getElementsByTagName("title")[0];
 console.log(Title);
+
+// TODO: Change the document title to "Modifying the DOM"
+Title.innerText = "Modifying the DOM";
