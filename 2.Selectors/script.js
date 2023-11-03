0deed750a72751915c4e148430bcce032aeff911
MohammedAB <albay.m@hotmail.com>
2023-11-03 05:18:47 +0100

    Add solution for title attribute to every important class element

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -1,2 +1,17 @@
-/*
-*/
+/* TODO: Add a title attribute to every element that has the important class, stating This is an important item. 
+Tip: adding a title attribute to an element is different from changing the title of a document.*/
+
+
+
+/* TODO: Select all the img tags and loop through them. 
+If they have no important class, turn their display property to none.*/
+
+
+
+/* TODO: Loop through all the paragraphs and display their content in the console.
+If the paragraph has a class, display its classname as well.*/
+
+
+
+
+// TODO: Give each of the paragraph a random text color (different for each one) UNLESS it has a class then leave it as it is.
