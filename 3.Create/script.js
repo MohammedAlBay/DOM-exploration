53855b78ec40f56ca7f5cddc9d48495d3a9568e5
MohammedAB <albay.m@hotmail.com>
2023-11-03 15:55:28 +0100

    Create section with random bgColor for each learner

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,2 +1,15 @@
 // TODO: Create a list with every learner of your group.
 const learners = ["Lucas", "Kilian", "Alec", "Mohamed", "Pieter", "Rebecca", "Alessandro", "Josué", "Thibault", "Eduarda", "Mohammed", "Funda", "Luis", "Alexandru", "Andrej", "Danté", "jonasi", "Kelsey", "Rana", "Jana", "Sieglinde", "Gustave", "Sezin", "Thierry", "Sylvie", "Anaïs"]
+// TODO: Create a new <section> with a random background-color for each learner in your group.
+learners.forEach(element => {
+  const sectionElement = document.createElement("section");
+
+  let r = Math.floor(Math.random()*256);
+  let g = Math.floor(Math.random()*256);
+  let b = Math.floor(Math.random()*256);
+
+  const bgColor = "rgb(" + r + "," + g + "," + b + ")";
+  
+  sectionElement.style.backgroundColor = bgColor;
+  console.log(sectionElement);
+});
