7bc16f2e0f630c8ac852be9dc3a3f47ca30ea5f3
MohammedAB <albay.m@hotmail.com>
2023-11-03 15:51:19 +0100

    Add array for learners of our group

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,2 +1,2 @@
-/*
-*/
+// TODO: Create a list with every learner of your group.
+const learners = ["Lucas", "Kilian", "Alec", "Mohamed", "Pieter", "Rebecca", "Alessandro", "Josué", "Thibault", "Eduarda", "Mohammed", "Funda", "Luis", "Alexandru", "Andrej", "Danté", "jonasi", "Kelsey", "Rana", "Jana", "Sieglinde", "Gustave", "Sezin", "Thierry", "Sylvie", "Anaïs"]
