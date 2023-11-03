637854634ae5ccdf39d02caff709c295e31afa46
MohammedAB <albay.m@hotmail.com>
2023-11-03 05:34:48 +0100

    Add solution to hide img elements with no important class

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -1,11 +1,20 @@
 /* TODO: Add a title attribute to every element that has the important class, stating This is an important item. 
 Tip: adding a title attribute to an element is different from changing the title of a document.*/
+const importantElements = document.getElementsByClassName("important");
 
-
+for (const element of importantElements){
+  element.setAttribute("title","This is an important item");
+}
 
 /* TODO: Select all the img tags and loop through them. 
 If they have no important class, turn their display property to none.*/
+const imgCollection = document.querySelectorAll("img");
 
+for(const img of imgCollection){
+  if (!img.classList.contains("important")){
+    img.style.display = "none";
+  }
+}
 
 
 /* TODO: Loop through all the paragraphs and display their content in the console.
