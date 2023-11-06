ffaf8dc81511e9718865c41c4ecbc670ba061aca
MohammedAB <albay.m@hotmail.com>
2023-11-06 11:06:52 +0100

    Add solution to place last child of Ol element before first child

--- 4.Navigate/script.js
+++ 4.Navigate/script.js
@@ -1,2 +1,9 @@
-/*
-*/
+// TODO: Select the last child of the `<ol>` tag and put it at the beginning of the list.
+const parentElement = document.querySelector("ol");
+const lastChild = parentElement.children[4]; // why does .lastChild property not work here to select the last child of Ol??
+parentElement.insertBefore(lastChild, parentElement.firstChild);
+
+// TODO: Move the `<h2>` of the third section in the second one and vice-versa.
+
+
+// TODO: Delete the last section from the DOM, we don't need it anyways.
