5cf9b2b9aac89ed3f008e49eb94e885aaa51a5ed
MohammedAB <albay.m@hotmail.com>
2023-11-06 11:49:44 +0100

    Add solution to replace h2 from section 2 & 3 with each other

--- 4.Navigate/script.js
+++ 4.Navigate/script.js
@@ -4,6 +4,13 @@ const lastChild = parentElement.children[4]; // why does .lastChild property not
 parentElement.insertBefore(lastChild, parentElement.firstChild);
 
 // TODO: Move the `<h2>` of the third section in the second one and vice-versa.
+const section2 = document.querySelectorAll("section")[1];
+const section3 = document.querySelectorAll("section")[2];
 
+const section2H2 = section2.children[0];
+const section3H2 = section3.children[0].children[0];
+const section3div = section3.children[0];
 
-// TODO: Delete the last section from the DOM, we don't need it anyways.
+section2.insertBefore(section3H2, section2.children[0]);
+section3div.insertBefore(section2H2, section3div.children[0]); 
+// // Why does this way not work ?? ---> section3.insertBefore(section2H2, section3.children[0].children[0]);
